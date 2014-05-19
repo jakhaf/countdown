c0d0d74d32b5c7bb3d9386f8d006bd618f09387e
Jakob <jakob.hafner@gmail.com>
2014-05-19 12:37:12 +0200

    added rounding and combination
    for days and hours - minutes and msecs will follow

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,5 +1,42 @@
-endDate = new Date('May 20, 2014 00:00:00');
+//Define the enddate
+var endDate = new Date('May 21, 2014 00:00:00');
 console.log(endDate);
 
-now = new Date();
+// get time derivates
+var now = new Date();
 console.log(now);
+
+var remMsec = endDate - now;
+console.log(remMsec + " ms");
+
+var remSec = remMsec * 0.001;
+console.log(remSec + " s");
+
+var remMin = remSec * 0.0166666667;
+console.log(remMin + " m");
+
+var remHr = remMin * 0.0166666667;
+console.log(remHr + " h");
+
+var remDay = remHr * 0.0416666667;
+console.log(remDay + " d");
+
+// rounding and combination
+
+if (remDay < 1) {
+	console.log("" + Math.floor(remHr) + " hours ");
+} else {
+	console.log("" + Math.floor(remDay) + " day(s), " + Math.floor(remHr-(Math.floor(remDay)*24)) + " hours");
+};
+
+
+
+
+
+
+console.log(remDay);
+
+if (remHr % 24 <= 0) {
+	console.log(remHr)
+}
+
